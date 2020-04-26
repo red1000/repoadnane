2769a905bd2ac2001a0b263d1d690c75d2d84279
Derouich Adnane <derouich.adnane0@gmail.com>
2020-04-26 21:56:56 +0000

    add address

--- adnaneproject/src/main/java/Adnane1.java
+++ adnaneproject/src/main/java/Adnane1.java
@@ -1,4 +1,13 @@
 package PACKAGE_NAME;
 
 public class Adnane1 {
+    private String adresse;
+
+    public String getAdresse() {
+        return adresse;
+    }
+
+    public void setAdresse(String adresse) {
+        this.adresse = adresse;
+    }
 }
--- adnaneproject/src/main/java/Adnane2.java
+++ adnaneproject/src/main/java/Adnane2.java
@@ -4,6 +4,7 @@ public class Adnane2 {
     private String name;
     private int age;
     private String lastName;
+    private String adresse;
 
     public String getName() {
         return "Adnane2";
@@ -24,6 +25,14 @@ public class Adnane2 {
 
     public String getLastName() {
         return "Derouich";
+    }
+
+    public String getAdresse() {
+        return adresse;
+    }
+
+    public void setAdresse(String adresse) {
+        this.adresse = adresse;
     }
 
     public void setLastName(String lastName) {
