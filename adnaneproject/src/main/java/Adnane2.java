b376ee78aab7fa64f320433cf0ffbecdaad4d58b
Red1 <red1.derouich@gmail.com>
2020-04-26 23:14:19 +0200

    #1256 [ADNANE] create project

--- adnaneproject/src/main/java/Adnane2.java
+++ adnaneproject/src/main/java/Adnane2.java
@@ -1,4 +1,13 @@
 package PACKAGE_NAME;
 
 public class Adnane2 {
+    private String name;
+
+    public String getName() {
+        return "Adnane2";
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
